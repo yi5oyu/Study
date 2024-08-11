a73543ebd1f89bcf87b8ed926a6e6b72f601dc83
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-08-12 04:46:24 +0900

    Update and rename .babelrc to babel.config.js

---
+++ React Native/Web/babel.config.js
@@ -0,0 +1,21 @@
+// Babel 설정 파일(.babelrc or babel.config.js)
+// JavaScript 코드 트랜스파일
+
+/* .babelrc
+// JSON 형태의 설정 파일
+{
+  "presets": ["@babel/preset-env", "@babel/preset-react"],
+  "plugins": ["babel-plugin-react-native-web"]
+}
+*/
+
+// babel.config.js
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react'
+  ],
+  plugins: [
+    'babel-plugin-react-native-web'
+  ]
+};
