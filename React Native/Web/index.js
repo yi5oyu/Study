5b59d523a2b523e9311ac9be3c9acea57fd00330
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-08-12 18:01:53 +0900

    Update index.js

--- React Native/Web/index.js
+++ React Native/Web/index.js
@@ -4,6 +4,7 @@ import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
 
+// index.web.js, app.web.js 를 나눠서 관리하고 싶지 않을때 Platform.OS 값으로 실행 시킴
 if (Platform.OS === 'web') {
   const rootTag = document.getElementById('root') || document.getElementById('main');
   AppRegistry.runApplication(appName, { initialProps: {}, rootTag });
