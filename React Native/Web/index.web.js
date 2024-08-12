dd7bd8b72d534f33b51b0cfc042a84bd0c180e63
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-08-12 18:02:13 +0900

    Update index.web.js

--- React Native/Web/index.web.js
+++ React Native/Web/index.web.js
@@ -1,13 +1,15 @@
 // npm install react-native-web react-dom 패키지 설치 후 웹에서 시작될 진입 파일 생성
 // root/index.web.js
 
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import { AppRegistry } from 'react-native-web';
+import App from './App.web';
 import { name as appName } from './app.json';
-import { render } from 'react-dom';
 
 AppRegistry.registerComponent(appName, () => App);
+
+const rootTag = document.getElementById('root') || document.getElementById('main');
 AppRegistry.runApplication(appName, {
   initialProps: {},
-  rootTag: document.getElementById('app-root'),
+  rootTag,
 });
