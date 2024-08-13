b24a9ee88ec25acfe75845c9c1a21d30a53a69bb
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-08-13 21:17:31 +0900

    Create B.jsx

---
+++ React.js/Components/B.jsx
@@ -0,0 +1,14 @@
+// React JS 구문 사용
+
+function B(){
+    const nothing1 = "SBeesdf";
+    const nothing2 = "gdgeDgE";
+
+    return(
+        <div>
+            <h2> { new Date().getFullYear() } : {nothing1}, {nothing2}</h2>
+        </div>
+    );
+}
+
+export default B
