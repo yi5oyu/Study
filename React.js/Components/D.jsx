a30d12e55edc25c393f2cf1f51eafb64ea94078c
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-08-15 03:31:26 +0900

    Update D.jsx

--- React.js/Components/D.jsx
+++ React.js/Components/D.jsx
@@ -3,7 +3,7 @@
 // 상위 컴포넌트: <D4 name="props test" nothing={15} check={true}/> 전달
 // 속성을 정의하지 않았을 경우 propTypes을 사용해 디폴트 값 설정
 // <D4 nothing={10}/>
-// prop-tupes 를 사용해 prop의 타입을 설정할 수 있음
+// prop-types 를 사용해 prop의 타입을 설정할 수 있음
 
 /*
 컴포넌트도 일반 태그 처럼 중첩해서 사용 수 있음
