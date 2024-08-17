0b9fe28a7e49633a45c199b3405213549147c84f
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-08-17 21:35:55 +0900

    Create ScrollView.jsx

---
+++ React Native/Components/ScrollView.jsx
@@ -0,0 +1,8 @@
+// 스크롤 가능한 뷰 컨테이너
+// horizontal, contentContainerStyle
+// horizontal: 수평 스크롤 여부를 지정
+// contentContainerStyle: 스크롤 뷰 내부의 콘텐츠 스타일을 지정
+
+<ScrollView>
+  <Text>Scrollable content</Text>
+</ScrollView>
