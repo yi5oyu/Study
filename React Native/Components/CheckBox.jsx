1ad157b96e9c3673c157bcee11f2c89ad9b69815
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-08-17 21:19:10 +0900

    Create CheckBox.jsx

---
+++ React Native/Components/CheckBox.jsx
@@ -0,0 +1,6 @@
+// 체크박스를 렌더링하는 컴포넌트
+// value, onValueChange
+// value: true or false 체크박스의 상태
+// onValueChange: 체크박스 변경될때 호출되는 콜백 함수
+
+<CheckBox value={isSelected} onValueChange={setSelection} />
