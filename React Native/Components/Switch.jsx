c44629676784c8373202394e403d47f59e4119f5
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-08-17 21:36:56 +0900

    Create Switch.jsx

---
+++ React Native/Components/Switch.jsx
@@ -0,0 +1,6 @@
+// 켜기/끄기 스위치를 제공하는 컴포넌트 (토글)
+// value, onValueChange
+// value: true or false (스위치의 현재 상태)
+// onValueChange: 스위치 상태가 변경될 때 호출되는 콜백 함수
+
+<Switch value={isEnabled} onValueChange={setIsEnabled} />
