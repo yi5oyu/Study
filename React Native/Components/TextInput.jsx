ea23ff29f95fe9d8b6363a9c69326fba3eeca744
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-08-17 21:48:11 +0900

    Create TextInput.jsx

---
+++ React Native/Components/TextInput.jsx
@@ -0,0 +1,7 @@
+// 텍스트를 입력할 수 있는 컴포넌트
+// placeholder, onChangeText, value
+// placeholder: 힌트 텍스트
+// onChangeText: 텍스트가 변경될 때 호출되는 콜백 함수
+// value: 현재 텍스트
+
+<TextInput placeholder="Enter text" onChangeText={(text) => setText(text)} />
