5d0bb357502279fe98837df0c7086254349a928a
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-08-28 01:36:46 +0900

    Create RedisConfig.java

---
+++ Redis/RedisConfig.java
@@ -0,0 +1,15 @@
+//
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+
+}
