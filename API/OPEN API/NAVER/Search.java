1e7c8ce2eab3279fb0640b3404ca2eb9c786785f
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-09-27 18:04:45 +0900

    Update and rename API 사용 to Search.java

--- API/OPEN API/NAVER/Search.java
+++ API/OPEN API/NAVER/Search.java
@@ -1,54 +1,46 @@
+// https://developers.naver.com/docs/serviceapi/search/blog/blog.md#java
+
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.net.*;
 import java.util.HashMap;
 import java.util.Map;
 
-
-public class ApiExamSearchBlog {
+public class ApiExamSearchLocal {
 
     public static void main(String[] args) {
-        String clientId = "YOUR_CLIENT_ID"; //애플리케이션 클라이언트 아이디
-        String clientSecret = "YOUR_CLIENT_SECRET"; //애플리케이션 클라이언트 시크릿
-
+        String clientId = "YOUR_CLIENT_ID"; // 애플리케이션 클라이언트 아이디
+        String clientSecret = "YOUR_CLIENT_SECRET"; // 애플리케이션 클라이언트 시크릿
 
         String text = null;
         try {
-            text = URLEncoder.encode("그린팩토리", "UTF-8");
+            text = URLEncoder.encode("식당", "UTF-8"); // 검색어 설정
         } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("검색어 인코딩 실패",e);
+            throw new RuntimeException("검색어 인코딩 실패", e);
         }
 
-
-        String apiURL = "https://openapi.naver.com/v1/search/blog?query=" + text;    // JSON 결과
-        //String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text; // XML 결과
-
+        String apiURL = "https://openapi.naver.com/v1/search/local.json?query=" + text; // JSON 결과
+        // String apiURL = "https://openapi.naver.com/v1/search/local.xml?query=" + text; // XML 결과
 
         Map<String, String> requestHeaders = new HashMap<>();
         requestHeaders.put("X-Naver-Client-Id", clientId);
         requestHeaders.put("X-Naver-Client-Secret", clientSecret);
-        String responseBody = get(apiURL,requestHeaders);
+        String responseBody = get(apiURL, requestHeaders);
 
-
-        System.out.println(responseBody);
+        System.out.println(responseBody); // 결과 출력
     }
 
-
-    private static String get(String apiUrl, Map<String, String> requestHeaders){
+    private static String get(String apiUrl, Map<String, String> requestHeaders) {
         HttpURLConnection con = connect(apiUrl);
         try {
             con.setRequestMethod("GET");
-            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+            for (Map.Entry<String, String> header : requestHeaders.entrySet()) {
                 con.setRequestProperty(header.getKey(), header.getValue());
             }
-
 
             int responseCode = con.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
                 return readBody(con.getInputStream());
-            } else { // 오류 발생
+            } else { // 에러 발생
                 return readBody(con.getErrorStream());
             }
         } catch (IOException e) {
@@ -58,32 +50,27 @@ public class ApiExamSearchBlog {
         }
     }
 
-
-    private static HttpURLConnection connect(String apiUrl){
+    private static HttpURLConnection connect(String apiUrl) {
         try {
             URL url = new URL(apiUrl);
-            return (HttpURLConnection)url.openConnection();
+            return (HttpURLConnection) url.openConnection();
         } catch (MalformedURLException e) {
-            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+            throw new RuntimeException("API URL이 잘못되었습니다: " + apiUrl, e);
         } catch (IOException e) {
-            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+            throw new RuntimeException("연결이 실패했습니다: " + apiUrl, e);
         }
     }
 
-
-    private static String readBody(InputStream body){
+    private static String readBody(InputStream body) {
         InputStreamReader streamReader = new InputStreamReader(body);
-
 
         try (BufferedReader lineReader = new BufferedReader(streamReader)) {
             StringBuilder responseBody = new StringBuilder();
-
 
             String line;
             while ((line = lineReader.readLine()) != null) {
                 responseBody.append(line);
             }
-
 
             return responseBody.toString();
         } catch (IOException e) {
