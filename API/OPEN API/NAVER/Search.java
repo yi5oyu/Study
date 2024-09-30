948b7a76fe3e30ae01f2bd8b7fed82ebe74a8aee
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-09-30 20:56:19 +0900

    Update Search.java

--- API/OPEN API/NAVER/Search.java
+++ API/OPEN API/NAVER/Search.java
@@ -1,80 +1,29 @@
 // https://developers.naver.com/docs/serviceapi/search/blog/blog.md#java
 
-import java.io.*;
-import java.net.*;
-import java.util.HashMap;
-import java.util.Map;
+@GetMapping(value = "/search", produces = "application/json; charset=UTF-8")
+    public ResponseEntity<String> naverSearchList() {
 
-public class ApiExamSearchLocal {
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query", "용인")
+                .queryParam("display", 5)
+                .queryParam("sort", "comment")
+                .encode(Charset.forName("UTF-8"))
+                .build()
+                .toUri();
 
-    public static void main(String[] args) {
-        String clientId = "YOUR_CLIENT_ID"; // 애플리케이션 클라이언트 아이디
-        String clientSecret = "YOUR_CLIENT_SECRET"; // 애플리케이션 클라이언트 시크릿
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", clientId)
+                .header("X-Naver-Client-Secret", clientSecret)
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .build();
 
-        String text = null;
-        try {
-            text = URLEncoder.encode("식당", "UTF-8"); // 검색어 설정
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("검색어 인코딩 실패", e);
-        }
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.exchange(req, String.class);
 
-        String apiURL = "https://openapi.naver.com/v1/search/local.json?query=" + text; // JSON 결과
-        // String apiURL = "https://openapi.naver.com/v1/search/local.xml?query=" + text; // XML 결과
+        String responseBody = responseEntity.getBody();
 
-        Map<String, String> requestHeaders = new HashMap<>();
-        requestHeaders.put("X-Naver-Client-Id", clientId);
-        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
-        String responseBody = get(apiURL, requestHeaders);
-
-        System.out.println(responseBody); // 결과 출력
+        return ResponseEntity.ok(responseBody);
     }
-
-    private static String get(String apiUrl, Map<String, String> requestHeaders) {
-        HttpURLConnection con = connect(apiUrl);
-        try {
-            con.setRequestMethod("GET");
-            for (Map.Entry<String, String> header : requestHeaders.entrySet()) {
-                con.setRequestProperty(header.getKey(), header.getValue());
-            }
-
-            int responseCode = con.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
-                return readBody(con.getInputStream());
-            } else { // 에러 발생
-                return readBody(con.getErrorStream());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("API 요청과 응답 실패", e);
-        } finally {
-            con.disconnect();
-        }
-    }
-
-    private static HttpURLConnection connect(String apiUrl) {
-        try {
-            URL url = new URL(apiUrl);
-            return (HttpURLConnection) url.openConnection();
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("API URL이 잘못되었습니다: " + apiUrl, e);
-        } catch (IOException e) {
-            throw new RuntimeException("연결이 실패했습니다: " + apiUrl, e);
-        }
-    }
-
-    private static String readBody(InputStream body) {
-        InputStreamReader streamReader = new InputStreamReader(body);
-
-        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
-            StringBuilder responseBody = new StringBuilder();
-
-            String line;
-            while ((line = lineReader.readLine()) != null) {
-                responseBody.append(line);
-            }
-
-            return responseBody.toString();
-        } catch (IOException e) {
-            throw new RuntimeException("API 응답을 읽는 데 실패했습니다.", e);
-        }
-    }
-}
