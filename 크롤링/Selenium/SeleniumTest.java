67c21fec9cd88a86cd2625161090854032a80f84
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-10-01 23:01:50 +0900

    Update SeleniumTest.java

--- 크롤링/Selenium/SeleniumTest.java
+++ 크롤링/Selenium/SeleniumTest.java
@@ -6,9 +6,11 @@ public class SeleniumTest {
 
         options.setBinary("C:/tools/chrome-headless-shell-win64/chrome-headless-shell.exe");
 
-        options.addArguments("--headless");
+        // GPU 하드웨어 가속 비활성화
         options.addArguments("--disable-gpu");
+        // Chrome 보안 샌드박스 비활성화 (권한 문제를 방지, 보안 약화됨)
         options.addArguments("--no-sandbox"); 
+        // 공유 메모리를 사용하지 못하도록 방지 (Docker 컨테이너에서 실행될때 발생할 수 있는 메모리 부족 및 충돌 발생 방지)
         options.addArguments("--disable-dev-shm-usage"); 
 
         WebDriver driver = new ChromeDriver(options);
