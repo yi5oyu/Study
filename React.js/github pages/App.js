28f069fef4e843cc7c14295e8dfbf1f07bbb0084
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-11-07 20:44:38 +0900

    Update App.js

--- React.js/github pages/App.js
+++ React.js/github pages/App.js
@@ -1,26 +1,21 @@
-import React from 'react'
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-import Portfolio from './pages/Portfolio/Portfolio'
-import Home from './pages/Home/Home'
-// import Layout from './pages/Layout/Layout'
+import Home from "./pages/Home/Home";
+import Test from "./pages/Test/Test";
 
 function App() {
-
   return (
     <HashRouter>
-        <Routes basename="/react">
-          <Route path="/" element={<Home />} />
-          <Route path="/portfolio" element={<Portfolio />}>
-          {/* <Route path="/layout" element={<Layout />}> */}
-            {/* <Route path=":cardId" element={<DetailCard />} /> */}
-          </Route>
-        </Routes>
+      <Routes basename="/react">
+        <Route path="/" element={<Home />} />
+        <Route path="/Test" element={<Test />}>
+          {/* <Route path=":cardId" element={<DetailCard />} /> */}
+        </Route>
+      </Routes>
     </HashRouter>
   );
-
 }
 
 export default App;
