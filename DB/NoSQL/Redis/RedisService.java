45c1ed770c574d7f6a4279402ea9f55c10d827ef
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-11-28 18:23:22 +0900

    Update RedisService.java

--- DB/NoSQL/Redis/RedisService.java
+++ DB/NoSQL/Redis/RedisService.java
@@ -4,14 +4,15 @@ public class RedisService {
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
+    // 데이터 저장
     public void saveValue(String key, String value) {
         redisTemplate.opsForValue().set(key, value);
     }
-
+    // 데이터 조회
     public String getValue(String key) {
         return (String) redisTemplate.opsForValue().get(key);
     }
-
+    // 데이터 삭제
     public void deleteValue(String key) {
         redisTemplate.delete(key);
     }
