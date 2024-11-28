e9b81c1d5ec6583a59113b27b04eb6f2bc67399d
Lee Gi-Young <111046436+yi5oyu@users.noreply.github.com>
2024-11-28 18:19:35 +0900

    Update RedisConfig.java

--- DB/NoSQL/Redis/RedisConfig.java
+++ DB/NoSQL/Redis/RedisConfig.java
@@ -12,17 +12,15 @@ public class RedisConfig {
     
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-    // RedisTemplate: Redis에 데이터를 저장하고 읽기 위한 클래스 (Redis 명령을 실행할 수 있는 메서드 제공)
-    // 키(String), 값(Object - 저장하는 데이터 타입)
-    // RedisConnectionFactory: Spring이 Redis 서버와 연결 관리
+        // RedisTemplate: Redis에 데이터를 저장하고 읽기 위한 클래스 (Redis 명령을 실행할 수 있는 메서드 제공)
+        // 키(String), 값(Object - 저장하는 데이터 타입)
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        
+        // RedisConnectionFactory: Spring이 Redis 서버와 연결 관리
         template.setConnectionFactory(connectionFactory);
-        // 
-        template.setKeySerializer(new StringRedisSerializer());
         // Redis에 저장되는 데이터 키를 직렬화
-        template.setValueSerializer(new StringRedisSerializer());
+        template.setKeySerializer(new StringRedisSerializer());
         // 값을 문자열로 직렬화
+        template.setValueSerializer(new StringRedisSerializer());
         return template;
     }
 
